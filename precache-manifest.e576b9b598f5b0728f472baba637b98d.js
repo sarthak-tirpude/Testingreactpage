0ffc24011714e4d7e688beaa4da416ff68a5d463
Sarthak Tirpude <sarthakt78@gmail.com>
2022-05-29 01:00:45 +0530

    Updates

---
+++ precache-manifest.e576b9b598f5b0728f472baba637b98d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2984413cd342b7812386",
+    "url": "/Testingreactpage/static/css/main.56005577.chunk.css"
+  },
+  {
+    "revision": "2984413cd342b7812386",
+    "url": "/Testingreactpage/static/js/main.38a34e59.chunk.js"
+  },
+  {
+    "revision": "ae49cecd317a1a2040b8",
+    "url": "/Testingreactpage/static/js/runtime~main.66a6c1c2.js"
+  },
+  {
+    "revision": "06d8b25f721bc6e283e3",
+    "url": "/Testingreactpage/static/js/2.0634139d.chunk.js"
+  },
+  {
+    "revision": "f940268ad7e0e0315aca095c8623364b",
+    "url": "/Testingreactpage/index.html"
+  }
+];
